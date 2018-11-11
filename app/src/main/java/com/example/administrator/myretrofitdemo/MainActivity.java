6d0f63ff0650a6c730b4fdebacd750db50aa469d
baogaijie <1006214269@qq.com>
2018-11-11 18:24:05 +0800

    Revert "a"
    This reverts commit 182c5046e60bd83bc84ef18d2f32730a161c06dd.

--- app/src/main/java/com/example/administrator/myretrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/myretrofitdemo/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // setContentView(R.layout.activity_main);
 
         String nam = "郑州分行/建设路支行";
         nam = nam.replaceAll("/", "\\.");
